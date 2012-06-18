c455c91b4292b79d65322c48094499dfa0dabdbb
Tony Dahbura <tony.dahbura@agilex.com>
2012-06-18 10:08:42 -0400

    Initial Commit

---
+++ iOS_AR_Ch3_LocationServices/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  iOS_AR_Ch3_LocationServices
+//
+//  Created by Tony Dahbura on 6/18/12.
+//  Copyright (c) 2012 Agilex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ iOS_AR_Ch3_LocationServices/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS_AR_Ch3_LocationServices
+//
+//  Created by Tony Dahbura on 6/18/12.
+//  Copyright (c) 2012 Agilex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
